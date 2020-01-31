e76dc949b6d97b39b0817a13545e83e63bf59aab
Arif <31542269+arifali6694@users.noreply.github.com>
2020-01-31 00:33:12 +0000

    Add files via upload

--- trees.h
+++ trees.h
@@ -1,42 +1,59 @@
 #include <iostream>
 
 template <typename T>
-struct Node{
+struct Node{//structs pulic by default
     Node(T value) : data(value), left(nullptr), right(nullptr) {}
     T data;
-    Node<T>* left;
-    Node<T>* right;
+    bool Insert(T);
+    bool search(T);
+    Node<T>* left;  //private
+    Node<T>* right; //private
     private:
     // std::unique_ptr<node<T>>
     // ...
 };
 
 template <typename T>
-struct BST{
+class BST{
+    public:
     BST(){};
-    Node<T>* Insert(T);
-    Node<T>* Insert(Node<T>*, T);
-    void Inorder(Node<T>*);
-    Node<T>* search(T);
-    Node<T>* search(Node<T>*, T);
-    Node<T>* root= nullptr; //root node
+    bool Insert(T);
+    void Inorder(Node<T>*);  //> void Inorder();
+    bool search(T);
+    Node<T>* root = nullptr; //private for Inorder();
     private:
+    //Node<T>* Insert(T);
+    //Node<T>* Insert(Node<T>*, T);
+    //Node<T>* search(Node<T>*, T);
 };
 
+
 template <typename T>
-Node<T>* BST<T>::Insert(T value){
-    if (!root){root = new Node<T>(value);return root;} // If root is NULL, insert root node
-    if (value > root->data){root->right = Insert(root->right, value);}
-    else{root->left = Insert(root->left, value);}
-    return root;
+bool BST<T>::Insert(T key){
+    if (root == nullptr){
+        root = new Node<T>(key);
+        return true;
+    }
+    return root->Insert(key); // != nullptr;
 }
 
 template <typename T>
-Node<T>* BST<T>::Insert(Node<T>* node, T value){
-    if (!node){return new Node<T>(value);}
-    if (value > node->data){node->right = Insert(node->right, value);}
-    else{node->left = Insert(node->left, value);}
-    return node;
+bool Node<T>::Insert(T key){
+    if (data == key){
+        return false;
+    }
+    if (data < key){
+        if (right == nullptr){
+            right = new Node<T>(key);
+            return true;
+        }
+        return right->Insert(key);
+    }
+    if (left == nullptr){
+            left = new Node<T>(key);
+            return true;
+        }
+    return left->Insert(key);
 }
 
 template <typename T>
@@ -48,27 +65,54 @@ void BST<T>::Inorder(Node<T>* node){
 }
 
 template <typename T>
-Node<T>* BST<T>::search(T key){
-    if (root != nullptr && root->data == key){
-        std::cout << "Node "<< key<< " at address :" << root << std::endl;
-        return root;}
+bool BST<T>::search(T key){
     if (root == nullptr){
-        std::cout<< key << " key not found" <<std::endl;
-        return nullptr;}
-    if (root->data < key){
-        return search(root->right, key);}
-    return search(root->left, key);
+        //std::cout<< key << " key not found" <<std::endl;
+        return false;
+    }
+    return root->search(key);// != nullptr;
 }
 
 template <typename T>
-Node<T>* BST<T>::search(Node<T>* node,T key){
-    if (node != nullptr && node->data == key){
-        std::cout << "Node "<< key<< " at address :" << node << std::endl;
-        return node;}
-    if (node == nullptr){
-        std::cout<< "Node "<< key << " not found" <<std::endl;
-        return nullptr;}
-    if (node->data < key){
-        return search(node->right, key);}
-    return search(node->left, key);
+bool Node<T>::search(T key){
+    if (data == NULL){
+        return false;//nullptr;
+    }
+    if (data == key){
+        //std::cout << "Node "<< key<< " at address :" << this << std::endl;
+        return true; //this;
+    }
+    if (data < key){
+        return right->search(key);
+    }
+    return left->search(key);
 }
+
+// template <typename T>
+// Node<T>* BST<T>::search(Node<T>* node,T key){
+//     if (node != nullptr && node->data == key){
+//         std::cout << "Node "<< key<< " at address :" << node << std::endl;
+//         return node;}
+//     if (node == nullptr){
+//         std::cout<< "Node "<< key << " not found" <<std::endl;
+//         return nullptr;}
+//     if (node->data < key){
+//         return search(node->right, key);}
+//     return search(node->left, key);
+// }
+
+// template <typename T>
+// Node<T>* BST<T>::Insert(T value){
+//     if (!root){root = new Node<T>(value);return root;} // If root is NULL, insert root node
+//     if (value > root->data){root->right = Insert(root->right, value);}
+//     else{root->left = Insert(root->left, value);}
+//     return root;
+// }
+
+// template <typename T>
+// Node<T>* BST<T>::Insert(Node<T>* node, T value){
+//     if (!node){return new Node<T>(value);}
+//     if (value > node->data){node->right = Insert(node->right, value);}
+//     else{node->left = Insert(node->left, value);}
+//     return node;
+// }
